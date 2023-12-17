ce064ad2c0bbdac70147bcd82e31e9e0ef18a52e
Lucas Flowers <lucasdflowers@gmail.com>
2023-12-17 00:23:30 -0700

    requirements.txt and build script update

--- build.ps1
+++ build.ps1
@@ -6,6 +6,7 @@ if ($clean) {
     Remove-Item -Recurse -Force dist
     Remove-Item -Recurse -Force build
 }
+pip install -r requirements.txt
 Get-ChildItem -Filter "*.spec" -Recurse | Remove-Item -Force
 Get-ChildItem -Path dist -Filter "*.exe" | Remove-Item -Force
 pyinstaller --onefile -n mc-utils src\main.py
